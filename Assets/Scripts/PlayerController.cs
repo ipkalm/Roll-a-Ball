28a7159e6fc8fc33f442234a374231b9219a95f0
Astro <astro@tuta.io>
2017-05-28 23:25:45 +0700

    Шар умеет катитсо

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+
+		rb.AddForce (new Vector3 (moveH, 0.0f, moveV) *  speed);
+	}
+}
