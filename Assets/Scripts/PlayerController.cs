806e8a1ed98df4657d5e1c35451f65892533e613
Astro <astro@tuta.io>
2017-05-30 21:20:16 +0700

    Добавлены объекты для сбора и их физика

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	public float speed;
 	private Rigidbody rb;
+	private int count;
 
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		this.count = 0;
 	}
-
-
-	// Update is called once per frame
+		
 	void FixedUpdate () {
 		float moveH = Input.GetAxis ("Horizontal");
 		float moveV = Input.GetAxis ("Vertical");
 
 		rb.AddForce (new Vector3 (moveH, 0.0f, moveV) *  speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive (false);
+			this.count++;
+		}
 	}
 }
