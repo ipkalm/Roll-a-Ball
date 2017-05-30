95f4902b0fd1be9a4dea08fedd6640365b1cf761
Astro <astro@tuta.io>
2017-05-31 02:08:47 +0700

    добавлен счётчик и текст победы

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
+	public Text countText;
+	public Text winText;
+	private int pickupsCount;
+
 	private Rigidbody rb;
 	private int count;
 
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		this.count = 0;
+		pickupsCount = GameObject.FindGameObjectsWithTag("PickUp").Length;
+		SetCoungText ();
+		winText.text = "";
 	}
 		
 	void FixedUpdate () {
@@ -23,6 +31,13 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ("PickUp")) {
 			other.gameObject.SetActive (false);
 			this.count++;
+			SetCoungText ();
+		}
+	}
+	void SetCoungText() {
+		countText.text = "Count :: " + this.count.ToString ();
+		if (this.count >= pickupsCount) {
+			winText.text = "You win!";
 		}
 	}
 }
